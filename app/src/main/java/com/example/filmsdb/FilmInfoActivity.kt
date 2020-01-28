4f02727df49351b3f1ab5ced9cf513d51c7cd921
Dmitry Nesterenko <dmitriy.nesterenko32@gmail.com>
2020-01-28 23:21:59 +0300

    version 1

---
+++ app/src/main/java/com/example/filmsdb/FilmInfoActivity.kt
@@ -0,0 +1,82 @@
+package com.example.filmsdb
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
+class FilmInfoActivity : AppCompatActivity() {
+
+    private var filmImage: Int = 0
+    private var filmDescription: Int = 0
+    private lateinit var filmLiked: CheckBox
+    private lateinit var filmComment: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_film_info)
+
+        filmImage = intent.getIntExtra(MainActivity.IMAGE_ID, 0)
+        filmDescription = intent.getIntExtra(MainActivity.FILM_DESCRIPTION, 0)
+
+        if (savedInstanceState != null) {
+            filmImage = savedInstanceState.getInt(MainActivity.IMAGE_ID)
+            filmDescription = savedInstanceState.getInt(MainActivity.FILM_DESCRIPTION)
+        }
+        updateFilmViews(filmImage, filmDescription)
+
+        val buttonInvite = findViewById<Button>(R.id.button_invite)
+        buttonInvite.setOnClickListener {
+            sendEmail(getString(R.string.invite_message))
+        }
+
+        filmLiked = findViewById(R.id.film_liked)
+        filmComment = findViewById(R.id.film_comment)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(MainActivity.IMAGE_ID, filmImage)
+        outState.putInt(MainActivity.FILM_DESCRIPTION, filmDescription)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        filmImage = savedInstanceState.getInt(MainActivity.IMAGE_ID)
+        filmDescription = savedInstanceState.getInt(MainActivity.FILM_DESCRIPTION)
+        updateFilmViews(filmImage, filmDescription)
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent()
+        intent.putExtra(MainActivity.IS_FILM_LIKED, filmLiked.isChecked)
+        intent.putExtra(MainActivity.FILM_COMMENT, filmComment.text.toString())
+        setResult(Activity.RESULT_OK, intent)
+        super.onBackPressed()
+    }
+
+    private fun updateFilmViews(filmImage: Int, filmDescription: Int) {
+
+        if (filmImage != 0) {
+            val filmImageView = findViewById<ImageView>(R.id.film_image)
+            filmImageView.setImageResource(filmImage)
+        }
+
+        if (filmDescription != 0) {
+            val filmDescriptionView = findViewById<TextView>(R.id.film_description)
+            filmDescriptionView.setText(filmDescription)
+        }
+    }
+
+    private fun sendEmail(textMessage: String) {
+        val sendIntent = Intent()
+        sendIntent.action = Intent.ACTION_SEND
+        sendIntent.putExtra(Intent.EXTRA_TEXT, textMessage)
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, "112233@gmail.com")
+        sendIntent.type = "text/plain"
+        sendIntent.resolveActivity(packageManager)?.let {
+            startActivity(sendIntent)
+        }
+    }
+}
--- app/src/main/java/com/example/filmsdb/MainActivity.kt
+++ app/src/main/java/com/example/filmsdb/MainActivity.kt
@@ -1,12 +1,120 @@
 package com.example.filmsdb
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+private const val LAST_SELECTED_FILM = "last_selected_film"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var ep5Header: TextView
+    private lateinit var ep7Header: TextView
+    private lateinit var ep8Header: TextView
+
+    private var lastSelectedFilm: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ep5Header = findViewById(R.id.ep_5_header)
+        ep7Header = findViewById(R.id.ep_7_header)
+        ep8Header = findViewById(R.id.ep_8_header)
+
+        val ep5Button = findViewById<Button>(R.id.ep_5_button)
+        ep5Button.setOnClickListener {
+            lastSelectedFilm = 1
+            updateFilmHeaderColor(lastSelectedFilm)
+            startFilmDescriptionActivity(R.drawable.episode_5, R.string.ep_5_description, this)
+        }
+
+        val ep7Button = findViewById<Button>(R.id.ep_7_button)
+        ep7Button.setOnClickListener {
+            lastSelectedFilm = 2
+            updateFilmHeaderColor(lastSelectedFilm)
+            startFilmDescriptionActivity(R.drawable.episode_7, R.string.ep_7_description, this)
+        }
+
+        val ep8Button = findViewById<Button>(R.id.ep_8_button)
+        ep8Button.setOnClickListener {
+            lastSelectedFilm = 3
+            updateFilmHeaderColor(lastSelectedFilm)
+            startFilmDescriptionActivity(R.drawable.episode_8, R.string.ep_8_description, this)
+        }
+    }
+
+    private fun startFilmDescriptionActivity(
+        imageId: Int,
+        filmDescription: Int,
+        activity: Activity
+    ) {
+        val intent = Intent(activity, FilmInfoActivity::class.java)
+        intent.putExtra(IMAGE_ID, imageId)
+        intent.putExtra(FILM_DESCRIPTION, filmDescription)
+        startActivityForResult(intent, FILM_INFO_REQUEST_CODE)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(LAST_SELECTED_FILM, lastSelectedFilm)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        lastSelectedFilm = savedInstanceState.getInt(LAST_SELECTED_FILM)
+        updateFilmHeaderColor(lastSelectedFilm)
+    }
+
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == FILM_INFO_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            var isLiked: Boolean? = null
+            var filmComment: String? = null
+
+            data?.let {
+                isLiked = data.getBooleanExtra(IS_FILM_LIKED, false)
+                filmComment = data.getStringExtra(FILM_COMMENT)
+            }
+
+            Log.d("Home Work", "Понравился фильм: $isLiked")
+            Log.d("Home Work", "Комментрай к фильму: $filmComment")
+        }
+    }
+
+    private fun updateFilmHeaderColor(header: Int) {
+        when (header) {
+            1 -> {
+                ep5Header.setTextColor(Color.BLUE)
+                ep7Header.setTextColor(Color.BLACK)
+                ep8Header.setTextColor(Color.BLACK)
+            }
+            2 -> {
+                ep5Header.setTextColor(Color.BLACK)
+                ep7Header.setTextColor(Color.BLUE)
+                ep8Header.setTextColor(Color.BLACK)
+            }
+            3 -> {
+                ep5Header.setTextColor(Color.BLACK)
+                ep7Header.setTextColor(Color.BLACK)
+                ep8Header.setTextColor(Color.BLUE)
+            }
+        }
+    }
+
+    companion object {
+        const val FILM_INFO_REQUEST_CODE = 1
+        const val IMAGE_ID = "image_id"
+        const val FILM_DESCRIPTION = "film_description"
+        const val IS_FILM_LIKED = "is_film_liked"
+        const val FILM_COMMENT = "film_comment"
     }
 }
